cd3913c986b4040e1f3c7be2a5c9432fa473b41f
renan <renand3z@gmail.com>
2023-09-01 17:01:25 +0200

    put, post, delete

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,37 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{get, web, App, HttpServer};
+use serde::{Serialize, Deserialize};
+use std::sync::Mutex;
+// use serde_json;
+mod todolist;
+use todolist::services;
+
+struct AppState {
+    todolist_entries: Mutex<Vec<TodolistEntry>>
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct TodolistEntry {
+    id: i32,
+    date: i64,
+    title: String,
+}
+
+#[get("/")]
+async fn index() -> String {
+   return "working".to_string();
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let app_data = web::Data::new(AppState {
+        todolist_entries: Mutex::new(vec![])
+});
 
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+        .app_data(app_data.clone())
+        .service(index)
+        .configure(services::config)
             .route("/", web::get().to(get_index))
             .route("/{id}", web::get().to(get_item)) 
     })
@@ -20,5 +47,3 @@ async fn get_index() -> String {
 async fn get_item(path: web::Path<u32>) -> String {
     format!("Item ID: {}", path) 
 }
-
-// test
